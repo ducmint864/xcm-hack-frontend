a4bedbb7ae961a535e100472910f0b3f04b65367
ducmint864 <nddminh2021@gmail.com>
2025-03-05 18:04:11 +0700

    feat: implement reserve transfer to people parachain

--- src/api/reserve.ts
+++ src/api/reserve.ts
@@ -1,8 +1,34 @@
-import { SS58String, Binary, AccountId, Enum } from "polkadot-api";
+import { SS58String, Enum } from "polkadot-api";
+import { PASEO_PEOPLE_CHAIN_ID } from "./people-chain";
+import { paseoAssetHubChainApi } from "./asset-hub-chain";
+import {
+  XcmV3Junctions,
+  XcmV3WeightLimit,
+  XcmV3Junction,
+} from "@polkadot-api/descriptors";
+import { getBeneficiary, getNativeAsset } from "../utils/";
 
+/**
+ *
+ * @notice Transfer tokens from paseo asset hub to paseo people chain
+ * @param address
+ * @param amount
+ */
 export const reserveTransferToParachain = (
   address: SS58String,
   amount: bigint
 ) => {
-  // Do sth cool
+  // Do sth cool soon
+  paseoAssetHubChainApi.tx.PolkadotXcm.limited_reserve_transfer_assets({
+    dest: Enum("V3", {
+      parents: 0,
+      interior: XcmV3Junctions.X1(
+        XcmV3Junction.Parachain(PASEO_PEOPLE_CHAIN_ID)
+      ),
+    }),
+    beneficiary: getBeneficiary(address),
+    assets: getNativeAsset(amount, 0),
+    fee_asset_item: 0,
+    weight_limit: XcmV3WeightLimit.Unlimited(),
+  });
 };
