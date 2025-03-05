c58d8348192e17bf90affa08d18b684843a5655f
ducmint864 <nddminh2021@gmail.com>
2025-03-05 16:13:05 +0700

    feat: add skeleton for reserve transfer api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export * from "./asset-hub-chain";
 export * from "./relay-chain";
 export * from "./teleport";
 export * from "./people-chain";
+export * from "./reserve";
---
+++ src/api/reserve.ts
@@ -0,0 +1,8 @@
+import { SS58String, Binary, AccountId, Enum } from "polkadot-api";
+
+export const reserveTransferToParachain = (
+  address: SS58String,
+  amount: bigint
+) => {
+  // Do sth cool
+};
